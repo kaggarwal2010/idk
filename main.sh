0f3e84c175256989fa0a2fecebcf957515bf548f
kaggarwal <121775729+kaggarwal2010@users.noreply.github.com>
2025-10-25 19:46:21 +0000

    setup

---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Opening Unblocked Browser"
+
+# Start Firefox with the correct display for VNC
+DISPLAY=:1 firefox https://kornineq.de/
---
+++ replit.nix
@@ -0,0 +1,6 @@
+{ pkgs }: {
+    deps = [
+        pkgs.firefox-esr
+        pkgs.bashInteractive
+    ];
+}
